b6e301e62e8005beb542e74ab84e0defdef30dbf
Viktor-sun <bobick.zdoh@gmail.com>
2021-10-03 19:27:43 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,7 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,7 +12,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let count = 0;
+
+  matrix.forEach((arr) => {
+    arr.forEach((_, j) => {
+      if (arr[j] === "^^") {
+        count += 1;
+      }
+    });
+  });
+  return count;
 }
